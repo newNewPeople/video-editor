40b2ea7ea5fbcfbe0071745cdcd80f6f7abde714
yangchao <>
2023-04-25 00:56:39 +0800

    no message

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:"./", //设置项目的根目录
-  build: {
-    outDir: 'docs' // 打包文件的输出目录
-  }
+  base:"/", //设置项目的根目录
 })
